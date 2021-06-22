cde1a6b568c7c7e30ec9ec7252c708a5f8652828
valladao <valladao@gmail.com>
2021-06-22 19:12:41 -0300

    Passing props to the component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 import Button from "./components/Button"
 
 function App() {
+
+  let count = 0
+
   return (
     <div className="App">
-      <Button />
+      <Button increment={1}/>
+      <Button increment={10}/>
+      <Button increment={100}/>
+      <Button increment={1000}/>
+      <span>{count}</span>
     </div>
   );
 }
